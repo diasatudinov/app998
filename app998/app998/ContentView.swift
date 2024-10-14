1c480c7389655428bed28106ac15e100a0e31d32
Dias <dias.atudinov@gmail.com>
2024-10-14 17:49:09 +0500

    first init

---
+++ app998/app998/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  app998
+//
+//  Created by Dias Atudinov on 14.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app998/app998/app998App.swift
@@ -0,0 +1,17 @@
+//
+//  app998App.swift
+//  app998
+//
+//  Created by Dias Atudinov on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct app998App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
