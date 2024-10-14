ce5673d5e40b80d0b5217eff02a5e6cabfa7df97
Dias <dias.atudinov@gmail.com>
2024-10-14 19:15:30 +0500

    new files for onboarding

---
+++ app998/app998/First/Onboarding/ReOnboardingUIView.swift
@@ -0,0 +1,18 @@
+//
+//  ReonboardingUIView.swift
+//  app998
+//
+//  Created by Dias Atudinov on 14.10.2024.
+//
+
+import SwiftUI
+
+struct ReonboardingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReonboardingUIView()
+}
---
+++ app998/app998/Second/Onboarding/UsOnboardingUIView.swift
@@ -0,0 +1,18 @@
+//
+//  UsOnboardingUIView.swift
+//  app998
+//
+//  Created by Dias Atudinov on 14.10.2024.
+//
+
+import SwiftUI
+
+struct UsOnboardingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsOnboardingUIView()
+}
