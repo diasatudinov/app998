b3ea204b89346f9056ac54c8c57607b9a153eb3e
Dias <dias.atudinov@gmail.com>
2024-10-14 19:19:24 +0500

    Loader View

--- app998/app998/LoaderView/LoaderView.swift
+++ app998/app998/LoaderView/LoaderView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct LoaderView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    @State private var isLoadingView: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoadingView {
+            ZStack {
+                Color.mainBG
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Image("logo998")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: UIScreen.main.bounds.width - 200)
+                        
+                        
+                    
+                    Spacer()
+                    Spacer()
+                    ZStack {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .secondaryBlue))
+                            .scaleEffect(x: 1.7, y: 1.7, anchor: .center)
+                            
+                            
+                    }
+                    .foregroundColor(.black)
+                    .padding(14)
+                    .padding(.bottom, 60)
+                }
+                .onAppear {
+                    startTimer()
+                }
+                .onDisappear {
+                    timer?.invalidate()
+                }
+                
+            }
+            
+        } else {
+            if true {
+                ReOnboardingUIView()
+            } else {
+               // UsOnboardingUIView()
+            }
+            
+        }
+    }
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+                isLoadingView.toggle()
+            }
+        }
     }
 }
 
