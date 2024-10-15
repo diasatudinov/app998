db3bc0ee32c573c0be29dda69880cc38616db105
Dias <dias.atudinov@gmail.com>
2024-10-15 17:16:52 +0500

    Discount model

--- app998/app998/First/Discounts/Model/Discount.swift
+++ app998/app998/First/Discounts/Model/Discount.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct Discount: Hashable, Codable {
+    var id = UUID()
+    var name: String
+    var market: String
+    var category: String
+    var startDate: Date
+    var endDate: Date
+    var note: String
+    
+    var isArchive: Bool {
+        get {
+            let currentDate = Date()
+            let calendar = Calendar.current
+            let orderDateTime = calendar.date(bySettingHour: calendar.component(.hour, from: endDate),
+                                              minute: calendar.component(.minute, from: endDate),
+                                              second: 0, of: endDate) ?? endDate
+            return orderDateTime < currentDate
+        }
+    }
+}
+
+struct Category: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+}
