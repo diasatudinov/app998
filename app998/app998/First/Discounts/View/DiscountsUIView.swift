8b4f50e554bdd242436158c46bfd049c50730fde
Dias <dias.atudinov@gmail.com>
2024-10-15 17:16:38 +0500

    new files add

---
+++ app998/app998/First/Discounts/View/DiscountsUIView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscountsUIView.swift
+//  app998
+//
+//  Created by Dias Atudinov on 15.10.2024.
+//
+
+import SwiftUI
+
+struct DiscountsUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiscountsUIView()
+}
---
+++ app998/app998/First/Discounts/ViewModel/DiscountsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DiscountsViewModel.swift
+//  app998
+//
+//  Created by Dias Atudinov on 15.10.2024.
+//
+
+import Foundation
