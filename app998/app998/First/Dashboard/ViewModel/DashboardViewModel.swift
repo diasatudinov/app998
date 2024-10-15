79f0f050490f031c94f0178263721b24b89c3018
Dias <dias.atudinov@gmail.com>
2024-10-15 15:00:49 +0500

    Dashboard view model

--- app998/app998/First/Dashboard/ViewModel/DashboardViewModel.swift
+++ app998/app998/First/Dashboard/ViewModel/DashboardViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class DashboardViewModel: ObservableObject {
+    
+    @Published var data: Dashboard = Dashboard(saved: 0, market: "Empty", categoryOne: "Not category", valueOne: 0, categoryTwo: "Not category", valueTwo: 0)
+    
+    
+    func editData(_ saved: Double, market: String, categoryOne: String, valueOne: Double, categoryTwo: String, valueTwo: Double) {
+        data.saved = saved
+        data.market = market
+        data.categoryOne = categoryOne
+        data.valueOne = valueOne
+        data.categoryTwo = categoryTwo
+        data.valueTwo = valueTwo
+    }
+}
+ 
