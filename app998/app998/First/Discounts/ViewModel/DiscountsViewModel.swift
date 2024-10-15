a5cf9c194179b56062a66ef7c8a5eb4bf7d85ffe
Dias <dias.atudinov@gmail.com>
2024-10-15 17:17:05 +0500

    Discount view model

--- app998/app998/First/Discounts/ViewModel/DiscountsViewModel.swift
+++ app998/app998/First/Discounts/ViewModel/DiscountsViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class DiscountsViewModel: ObservableObject {
+    
+    @Published var discounts: [Discount] = [Discount(name: "", market: "", category: "", startDate: Date(), endDate: Date(), note: "")]
+    @Published var categories: [String] = [
+        "Rock",
+        "Pop",
+        "Jazz",
+        "Hip-Hop"
+    ]
+    
+    @Published var selectedCategory: String?
+    
+    func filteredDiscounts() -> [Discount] {
+        if let selectedCategory = selectedCategory {
+            return discounts.filter { $0.category.lowercased() == selectedCategory.lowercased() }
+        }
+        return discounts
+    }
+}
