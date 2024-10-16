51cb84e644f272a31232edb06bd7c0a9f26359a0
Dias <dias.atudinov@gmail.com>
2024-10-16 15:31:50 +0500

    new Files

---
+++ app998/app998/First/Discounts/View/EditDiscountUIView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDiscountUIView.swift
+//  app998
+//
+//  Created by Dias Atudinov on 16.10.2024.
+//
+
+import SwiftUI
+
+struct EditDiscountUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDiscountUIView()
+}
