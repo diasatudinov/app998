ca9a3778318c6f6b5f2273ced0cb0ff628998fad
Dias <dias.atudinov@gmail.com>
2024-10-16 15:32:41 +0500

    Create discount view

--- app998/app998/First/Discounts/View/CreateDiscountUIView.swift
+++ app998/app998/First/Discounts/View/CreateDiscountUIView.swift
@@ -8,11 +8,237 @@
 import SwiftUI
 
 struct CreateDiscountUIView: View {
+    
+    @ObservedObject var viewModel: DiscountsViewModel
+    
+    @State var name = ""
+    @State var market = ""
+    @State var category = ""
+    @State var startDate = Date()
+    @State var endDate = Date()
+    @State var note = ""
+    
+    @State var startDatePickerShow = false
+    @State var endDatePickerShow = false
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.secondBG.ignoresSafeArea(edges: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                
+                ZStack {
+                    Color.mainBG
+                    HStack {
+                        Text("Add discount")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(.white)
+                    }
+                    HStack {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Text("Cancel")
+                                .foregroundColor(.secondaryBlue)
+                        }
+                        Spacer()
+                    }.padding(.horizontal)
+                }.frame(height: 48)
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 12) {
+                        
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            TextField("Discount 2 for the price of 1", text: $name)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.white.opacity(0.08))
+                                .cornerRadius(12)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                )
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Market")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            TextField("Domino's Pizza", text: $market)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.white.opacity(0.08))
+                                .cornerRadius(12)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                )
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Category")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            TextField("Food", text: $category)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.white.opacity(0.08))
+                                .cornerRadius(12)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                )
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Start date")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            VStack {
+                                HStack {
+                                    Text("\(formattedDate(date: startDate))")
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .onTapGesture {
+                                            startDatePickerShow.toggle()
+                                        }
+                                    //.frame(maxWidth: .infinity)
+                                    
+                                    Spacer()
+                                    
+                                }.background(Color.white.opacity(0.08))
+                                    .cornerRadius(12)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 12)
+                                            .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                    )
+                                if startDatePickerShow {
+                                    
+                                    DatePicker(
+                                        "Choose your date",
+                                        selection: $startDate,
+                                        displayedComponents: [.date]
+                                    )
+                                    .datePickerStyle(.wheel)
+                                    .labelsHidden()
+                                    .background(Color.clear)
+                                    
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Finish date")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            VStack {
+                                HStack {
+                                    Text("\(formattedDate(date: endDate))")
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .onTapGesture {
+                                            endDatePickerShow.toggle()
+                                        }
+                                    //.frame(maxWidth: .infinity)
+                                    
+                                    Spacer()
+                                    
+                                }.background(Color.white.opacity(0.08))
+                                    .cornerRadius(12)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 12)
+                                            .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                    )
+                                if endDatePickerShow {
+                                    
+                                    DatePicker(
+                                        "Choose your date",
+                                        selection: $endDate,
+                                        displayedComponents: [.date]
+                                    )
+                                    .datePickerStyle(.wheel)
+                                    .labelsHidden()
+                                    .background(Color.clear)
+                                    
+                                }
+                            }
+                        }
+                        
+                
+                        
+                        VStack(alignment: .leading) {
+                            Text("Note")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                            
+                            
+                            TextEditor(text: $note)
+                                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                                .background(Color.white.opacity(0.08))
+                                .cornerRadius(12)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1).foregroundColor(.white.opacity(0.08))
+                                )
+                            
+                        }
+                        
+                        
+                    }.padding(.horizontal).padding(.top, 24)
+                    
+                    
+                }
+                Button {
+                    if !name.isEmpty, !market.isEmpty, !category.isEmpty, !note.isEmpty {
+                        let discount = Discount(name: name, market: market, category: category, startDate: startDate, endDate: endDate, note: note)
+                        viewModel.addDiscount(discount)
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 90)
+                            .cornerRadius(20)
+                            .foregroundColor(.primaryBlue)
+                        
+                        HStack(spacing: 12) {
+                            Text("Create")
+                                .font(.system(size: 17, weight: .medium))
+                                .foregroundColor(.white)
+                            ZStack {
+                                Circle()
+                                    .frame(height: 38)
+                                    .foregroundColor(.white.opacity(0.3))
+                                Image(systemName: "checkmark")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 15)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                }
+                
+            }.ignoresSafeArea(edges: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    private func formattedDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.YYYY"
+        return formatter.string(from: date)
     }
 }
 
+
 #Preview {
-    CreateDiscountUIView()
+    CreateDiscountUIView(viewModel: DiscountsViewModel())
 }
+
+
--- app998/app998/First/Discounts/ViewModel/DiscountsViewModel.swift
+++ app998/app998/First/Discounts/ViewModel/DiscountsViewModel.swift
@@ -9,14 +9,83 @@ import Foundation
 
 class DiscountsViewModel: ObservableObject {
     
-    @Published var discounts: [Discount] = [Discount(name: "", market: "", category: "", startDate: Date(), endDate: Date(), note: ""), Discount(name: "Discount 2 for the price of 1", market: "Domino's Pizza", category: "Food", startDate: Date(), endDate: Calendar.current.date(byAdding: .hour, value: 48, to: Date()) ?? Date(), note: "Buy two pizzas and get the second one for free asdsadasd sadsad")]
-    @Published var categories: [String] = [
-        "Rock",
-        "Pop",
-        "Jazz",
-        "Hip-Hop"
-    ]
+    @Published var discounts: [Discount] = []{
+        didSet {
+            saveDiscount()
+        }
+    }
     
+    @Published var categories: [String] = [] {
+        didSet {
+            saveCategory()
+        }
+    }
+    
+    private let fileNameCategory = "Category.json"
+    private let fileNameDiscount = "Discount.json"
+    
+    init() {
+        loadCategory()
+        loadDiscount()
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    private func filePathCategory() -> URL {
+        return getDocumentsDirectory().appendingPathComponent(fileNameCategory)
+    }
+    
+    private func filePathDiscount() -> URL {
+        return getDocumentsDirectory().appendingPathComponent(fileNameDiscount)
+    }
+   
+    
+    private func saveCategory() {
+        DispatchQueue.global().async {
+            let encoder = JSONEncoder()
+            do {
+                let data = try encoder.encode(self.categories)
+                try data.write(to: self.filePathCategory())
+            } catch {
+                print("Failed to save players: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func saveDiscount() {
+        DispatchQueue.global().async {
+            let encoder = JSONEncoder()
+            do {
+                let data = try encoder.encode(self.discounts)
+                try data.write(to: self.filePathDiscount())
+            } catch {
+                print("Failed to save players: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func loadDiscount() {
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: filePathDiscount())
+            self.discounts = try decoder.decode([Discount].self, from: data)
+        } catch {
+            print("Failed to load players: \(error.localizedDescription)")
+        }
+    }
+    
+    private func loadCategory() {
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: filePathCategory())
+            self.categories = try decoder.decode([String].self, from: data)
+        } catch {
+            print("Failed to load players: \(error.localizedDescription)")
+        }
+    }
     @Published var selectedCategory: String?
     
     func filteredDiscounts() -> [Discount] {
@@ -25,4 +94,26 @@ class DiscountsViewModel: ObservableObject {
         }
         return discounts
     }
+    
+    func addDiscount(_ discount: Discount) {
+        discounts.append(discount)
+    }
+    
+    func deleteDiscount(_ discount: Discount) {
+        if let index = discounts.firstIndex(where: { $0.id == discount.id }) {
+            discounts.remove(at: index)
+        }
+    }
+    
+    func editDiscount(_ discount: Discount, name: String, market: String, category: String, startDate: Date, endDate: Date, note: String) {
+        if let index = discounts.firstIndex(where: { $0.id == discount.id }) {
+            discounts[index].name = name
+            discounts[index].market = market
+            discounts[index].category = category
+            discounts[index].startDate = startDate
+            discounts[index].endDate = endDate
+            discounts[index].note = note
+        }
+    }
 }
+
